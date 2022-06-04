6ab401b10673c973598dab069a0e246237127dc1
Rafael Mercedes <rafaelmersant@gmail.com>
2022-06-04 07:33:33 -0400

    Add upload products script and file

---
+++ upload_products.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os, csv
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MultiServices.settings")
+
+import django
+django.setup()
+
+from products.models import Product, ProductCategory
+from administration.models import Company
+
+def add_new_product(description, price, cost):
+    description = description.strip()
+
+    try:
+        product = Product()
+        product.description = description
+        product.price = price
+        product.cost = cost
+        product.itbis = 0
+        product.category = ProductCategory.objects.filter(description='GENERICA')
+        product.company = Company.objects.get(pk=14)
+        product.save()
+        print("*** product: ", description, ' -> added!')
+    except:
+        print('There was an error trying to add this product: ', description)
+        
+# Main Process        
+with open('productsFull.csv', newline='') as csvfile:
+    musical_works = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for index, row in enumerate(musical_works):
+        add_new_product(row[0], row[1], row[2])
